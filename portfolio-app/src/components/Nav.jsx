40c545d5a546c2b80d8e508cac6c8b8d0eec6d70
Brandon <bbrungardt02@gmail.com>
2023-08-09 21:09:59 -0500

    scroll effect on nav

--- portfolio-app/src/components/Nav.jsx
+++ portfolio-app/src/components/Nav.jsx
@@ -1,12 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Nav() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const navContainer = document.querySelector(".nav-container");
+      const scrollOffset = window.scrollY;
+      navContainer.style.backgroundPosition = `center top ${
+        -scrollOffset * 0.5
+      }px`;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div>
-      <nav>
-        <Link to="/">About</Link>
-        <Link to="/projects"> Projects</Link>
+    <div className="nav-container">
+      <nav className="fancy-nav">
+        <NavLink to="/" className="nav-link" activeClassName="active">
+          About
+        </NavLink>
+        <NavLink to="/projects" className="nav-link" activeClassName="active">
+          Projects
+        </NavLink>
+        <NavLink to="/contact" className="nav-link" activeClassName="active">
+          Contact
+        </NavLink>
       </nav>
     </div>
   );
